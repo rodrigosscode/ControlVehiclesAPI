70031a41746575da2a79935b139bade735df383f
rodrigosscode <rodrigosilvasantos2@gmail.com>
2021-05-21 12:25:51 -0300

    Inicialização do projeto

---
+++ src/main/java/br/com/zup/controlvehicles/ControlvehiclesApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.controlvehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlvehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlvehiclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/controlvehicles/ControlvehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.controlvehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlvehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
